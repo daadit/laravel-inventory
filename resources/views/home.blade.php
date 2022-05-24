cf7902d0401c9590c3074d88c96a9dd59de02c12
adilsanak <adilsaputra0101@gmail.com>
2022-05-24 13:37:37 +0700

    Home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,258 @@
 @extends('layouts.app')
 
+@section('menu')
+
+<ul class="pcoded-item pcoded-left-item">
+    <li class="active">
+        <a href="navbar-light.htm">
+            <span class="pcoded-micon"><i class="feather icon-home"></i></span>
+            <span class="pcoded-mtext">Dashboard</span>
+        </a>
+    </li>
+    <li class="pcoded-hasmenu">
+        <a href="javascript:void(0)">
+            <span class="pcoded-micon"><i class="feather icon-layers"></i></span>
+            <span class="pcoded-mtext">Masters</span>
+        </a>
+        <ul class="pcoded-submenu">
+            <li class=" pcoded-hasmenu">
+                <a href="javascript:void(0)">
+                    <span class="pcoded-mtext">Vertical</span>
+                </a>
+                <ul class="pcoded-submenu">
+                    <li class=" ">
+                        <a href="menu-static.htm">
+                            <span class="pcoded-mtext">Static Layout</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-header-fixed.htm">
+                            <span class="pcoded-mtext">Header Fixed</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-compact.htm">
+                            <span class="pcoded-mtext">Compact</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-sidebar.htm">
+                            <span class="pcoded-mtext">Sidebar Fixed</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </li>
+            <li class=" pcoded-hasmenu">
+                <a href="javascript:void(0)">
+                    <span class="pcoded-mtext">Horizontal</span>
+                </a>
+                <ul class="pcoded-submenu">
+                    <li class=" ">
+                        <a href="menu-horizontal-static.htm" target="_blank">
+                            <span class="pcoded-mtext">Static Layout</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-horizontal-fixed.htm" target="_blank">
+                            <span class="pcoded-mtext">Fixed layout</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-horizontal-icon.htm" target="_blank">
+                            <span class="pcoded-mtext">Static With Icon</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-horizontal-icon-fixed.htm" target="_blank">
+                            <span class="pcoded-mtext">Fixed With Icon</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class=" ">
+                <a href="menu-bottom.htm">
+                    <span class="pcoded-mtext">Bottom Menu</span>
+                </a>
+            </li>
+            <li class=" ">
+                <a href="box-layout.htm" target="_blank">
+                    <span class="pcoded-mtext">Box Layout</span>
+                </a>
+            </li>
+            <li class=" ">
+                <a href="menu-rtl.htm" target="_blank">
+                    <span class="pcoded-mtext">RTL</span>
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li class="pcoded-hasmenu">
+        <a href="javascript:void(0)">
+            <span class="pcoded-micon"><i class="feather icon-clipboard"></i></span>
+            <span class="pcoded-mtext">Transactions</span>
+        </a>
+        <ul class="pcoded-submenu">
+            <li class=" pcoded-hasmenu">
+                <a href="javascript:void(0)">
+                    <span class="pcoded-mtext">Vertical</span>
+                </a>
+                <ul class="pcoded-submenu">
+                    <li class=" ">
+                        <a href="menu-static.htm">
+                            <span class="pcoded-mtext">Static Layout</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-header-fixed.htm">
+                            <span class="pcoded-mtext">Header Fixed</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-compact.htm">
+                            <span class="pcoded-mtext">Compact</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-sidebar.htm">
+                            <span class="pcoded-mtext">Sidebar Fixed</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </li>
+            <li class=" pcoded-hasmenu">
+                <a href="javascript:void(0)">
+                    <span class="pcoded-mtext">Horizontal</span>
+                </a>
+                <ul class="pcoded-submenu">
+                    <li class=" ">
+                        <a href="menu-horizontal-static.htm" target="_blank">
+                            <span class="pcoded-mtext">Static Layout</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-horizontal-fixed.htm" target="_blank">
+                            <span class="pcoded-mtext">Fixed layout</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-horizontal-icon.htm" target="_blank">
+                            <span class="pcoded-mtext">Static With Icon</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-horizontal-icon-fixed.htm" target="_blank">
+                            <span class="pcoded-mtext">Fixed With Icon</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class=" ">
+                <a href="menu-bottom.htm">
+                    <span class="pcoded-mtext">Bottom Menu</span>
+                </a>
+            </li>
+            <li class=" ">
+                <a href="box-layout.htm" target="_blank">
+                    <span class="pcoded-mtext">Box Layout</span>
+                </a>
+            </li>
+            <li class=" ">
+                <a href="menu-rtl.htm" target="_blank">
+                    <span class="pcoded-mtext">RTL</span>
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li class="pcoded-hasmenu">
+        <a href="javascript:void(0)">
+            <span class="pcoded-micon"><i class="feather icon-file-minus"></i></span>
+            <span class="pcoded-mtext">Reports</span>
+        </a>
+        <ul class="pcoded-submenu">
+            <li class=" pcoded-hasmenu">
+                <a href="javascript:void(0)">
+                    <span class="pcoded-mtext">Vertical</span>
+                </a>
+                <ul class="pcoded-submenu">
+                    <li class=" ">
+                        <a href="menu-static.htm">
+                            <span class="pcoded-mtext">Static Layout</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-header-fixed.htm">
+                            <span class="pcoded-mtext">Header Fixed</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-compact.htm">
+                            <span class="pcoded-mtext">Compact</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-sidebar.htm">
+                            <span class="pcoded-mtext">Sidebar Fixed</span>
+                        </a>
+                    </li>
+
+                </ul>
+            </li>
+            <li class=" pcoded-hasmenu">
+                <a href="javascript:void(0)">
+                    <span class="pcoded-mtext">Horizontal</span>
+                </a>
+                <ul class="pcoded-submenu">
+                    <li class=" ">
+                        <a href="menu-horizontal-static.htm" target="_blank">
+                            <span class="pcoded-mtext">Static Layout</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-horizontal-fixed.htm" target="_blank">
+                            <span class="pcoded-mtext">Fixed layout</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-horizontal-icon.htm" target="_blank">
+                            <span class="pcoded-mtext">Static With Icon</span>
+                        </a>
+                    </li>
+                    <li class=" ">
+                        <a href="menu-horizontal-icon-fixed.htm" target="_blank">
+                            <span class="pcoded-mtext">Fixed With Icon</span>
+                        </a>
+                    </li>
+                </ul>
+            </li>
+            <li class=" ">
+                <a href="menu-bottom.htm">
+                    <span class="pcoded-mtext">Bottom Menu</span>
+                </a>
+            </li>
+            <li class=" ">
+                <a href="box-layout.htm" target="_blank">
+                    <span class="pcoded-mtext">Box Layout</span>
+                </a>
+            </li>
+            <li class=" ">
+                <a href="menu-rtl.htm" target="_blank">
+                    <span class="pcoded-mtext">RTL</span>
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li class="">
+        <a href="navbar-light.htm">
+            <span class="pcoded-micon"><i class="feather icon-menu"></i></span>
+            <span class="pcoded-mtext">Navigation</span>
+        </a>
+    </li>
+</ul>
+
+@endsection
+
 @section('content')
 
 <div class="pcoded-content">
