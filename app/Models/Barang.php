d8ac9fd808cae32f58321c1a27087d4e939ee44a
adilsanak <adilsaputra0101@gmail.com>
2022-05-25 15:50:44 +0700

    Barang

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -4,8 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Barang extends Model
 {
     use HasFactory;
+
+    public function list()
+    {
+        return DB::table('barang')->get();
+    }
+
+    public function saveData($data)
+    {
+        DB::table('barang')->insert($data);
+    }
+
+    public function updateData($id, $data)
+    {
+        DB::table('barang')
+            ->where('id', '=', $id)
+            ->update($data);
+    }
+
+    public function deleteData($id)
+    {
+        DB::table('barang')
+            ->where('id', '=', $id)
+            ->delete();
+    }
 }
