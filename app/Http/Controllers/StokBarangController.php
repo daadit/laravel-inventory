8ebd2c1612058ee1f571b274835696a7d24b596f
adilsanak <adilsaputra0101@gmail.com>
2022-06-04 06:16:24 +0700

    Stokbarang

--- app/Http/Controllers/StokBarangController.php
+++ app/Http/Controllers/StokBarangController.php
@@ -2,9 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\StokBarang;
 use Illuminate\Http\Request;
 
 class StokBarangController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->stokbarang = new StokBarang();
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $data = [
+            'stokbarang' => $this->stokbarang->list()
+        ];
+        return view('stok-barang', $data);
+    }
 }
